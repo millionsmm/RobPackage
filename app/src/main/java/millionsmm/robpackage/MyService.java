9917b30ed494136243f2d16c34cedfbb7509913a
millionsmm <linweipeng96@qq.com>
2017-10-24 17:54:32 +0800

    注册AccessibilityService服务

---
+++ app/src/main/java/millionsmm/robpackage/MyService.java
@@ -0,0 +1,16 @@
+package millionsmm.robpackage;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
