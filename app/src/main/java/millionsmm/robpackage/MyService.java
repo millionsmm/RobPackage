1f5056305549f86cb161534cc62024f945bc1dff
millionsmm <linweipeng96@qq.com>
2017-10-24 18:11:04 +0800

    通知栏，window状态改变，window内容改变事件

--- app/src/main/java/millionsmm/robpackage/MyService.java
+++ app/src/main/java/millionsmm/robpackage/MyService.java
@@ -6,7 +6,22 @@ import android.view.accessibility.AccessibilityEvent;
 public class MyService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        int eventType = event.getEventType();
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                String className = event.getClassName().toString();
+                if (className.equals("com.tencent.mm.ui.LauncherUI")) {
 
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
+
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")) {
+
+                }
+                break;
+        }
     }
 
     @Override
